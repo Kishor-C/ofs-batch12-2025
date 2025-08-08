796e5c63aa6520384ca290d2f0c7834321279519
Kishor <kishor1124@gmail.com>
2025-08-08 10:00:53 +0530

    db util class

---
+++ eclipse-workspace/java-case-study/src/com/oracle/dao/EmployeeDaoJdbcImpl.java
@@ -0,0 +1,39 @@
+package com.oracle.dao;
+
+import java.util.List;
+
+import com.oracle.beans.Employee;
+
+public class EmployeeDaoJdbcImpl implements EmployeeDAO {
+
+	@Override
+	public Employee save(Employee employee) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Employee> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Employee employee) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Employee update(Employee employee) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
