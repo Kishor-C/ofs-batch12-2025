545c69a56851163a0e68a8a1f717175ad87360ec
Kishor <kishor1124@gmail.com>
2025-08-12 11:22:09 +0530

    jms programs

---
+++ jms-programs/Reciever.java
@@ -0,0 +1,45 @@
+
+import java.util.Hashtable;
+
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueReceiver;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import weblogic.jndi.WLInitialContextFactory;
+
+public class Reciever {
+	public static void main(String[] args) {
+		try {
+			Hashtable<String, String> env = new Hashtable<String, String>();
+			env.put(Context.PROVIDER_URL, "t3://localhost:7001");
+			env.put(Context.INITIAL_CONTEXT_FACTORY, WLInitialContextFactory.class.getName()); 
+			Context ctx = new InitialContext(env);
+			
+			QueueConnectionFactory qcf = (QueueConnectionFactory) ctx.lookup("MYCF");
+			Queue queue = (Queue) ctx.lookup("MYQ");
+			
+			QueueConnection con = qcf.createQueueConnection();
+			con.start();
+			QueueSession session = con.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+			QueueReceiver receiver = session.createReceiver(queue);
+			
+			TextMessage msg = (TextMessage) receiver.receive(60000);
+			System.out.println("Your recieved: "+msg.getText());
+
+			
+			
+			session.close();
+			con.close();
+
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ jms-programs/Sender.java
@@ -0,0 +1,44 @@
+
+import java.util.Hashtable;
+
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueSender;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import weblogic.jndi.WLInitialContextFactory;
+public class Sender {
+	public static void main(String[] args) {
+		try {
+			Hashtable<String, String> env = new Hashtable<String, String>();
+			env.put(Context.PROVIDER_URL, "t3://localhost:7001");
+			env.put(Context.INITIAL_CONTEXT_FACTORY, WLInitialContextFactory.class.getName()); 
+			Context ctx = new InitialContext(env);
+			
+			QueueConnectionFactory qcf = (QueueConnectionFactory) ctx.lookup("MYCF");
+			Queue queue = (Queue) ctx.lookup("MYQ");
+			
+			QueueConnection con = qcf.createQueueConnection();
+			con.start();
+			QueueSession session = con.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+			
+			QueueSender sender = session.createSender(queue);
+
+			TextMessage msg = session.createTextMessage();
+			msg.setText(args[0]);
+			sender.send(msg);
+			
+			System.out.println("Message Sent:-");
+			session.close();
+			con.close();
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
