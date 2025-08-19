9d1c9f653337eb74eb4b439f306428059b0115c7
Kishor <kishor1124@gmail.com>
2025-08-19 13:09:19 +0530

    employee controller

--- eclipse-workspace/spring-boot-demo.zip_expanded/spring-boot-demo/src/main/java/com/oracle/controller/EmployeeRestApi.java
+++ eclipse-workspace/spring-boot-demo.zip_expanded/spring-boot-demo/src/main/java/com/oracle/controller/EmployeeRestApi.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,41 @@ import org.springframework.web.bind.annotation.RestController;
 import com.oracle.beans.Employee;
 import com.oracle.business.EmployeeService;
 
+
+
 @RestController
 @RequestMapping("/v1/employee/api")
 public class EmployeeRestApi {
 	@Autowired
 	private EmployeeService serviceEmployee;
+	
+	/*
+	 * 2 ways you can send the data when you update
+	 * 1. through param - all the data comes in pathaparam
+	 * 2. through JSON - where id comes from pathparam
+	 */
+	
+	
+	@PutMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE )
+	public ResponseEntity<Object> updateEmployee(@RequestBody Employee emp, @PathVariable("id") int id) {
+		
+		try {
+			Employee updatedEmp = null;
+			if(emp.getName() != null) {
+				updatedEmp = serviceEmployee.updateEmployeeNameById(id, emp.getName());
+			}
+			if(emp.getDob() != null) {
+				updatedEmp = serviceEmployee.updateEmployeeDobById(id, emp.getDob());
+			}
+			return ResponseEntity.status(200).body(updatedEmp);
+		} catch(RuntimeException e) {
+			String err = e.getMessage();
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("message", err); 
+			return ResponseEntity.status(404).body(map);
+		}
+	}
+	
 	// to store the data
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Object> save(@RequestBody Employee emp) {
